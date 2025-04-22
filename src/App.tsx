28f3074984e7c7a71cbc49b05be6cf686f9aca51
dstet <me@dstet.me>
2025-04-21 21:28:30 -0400

    Fix up post-merge

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { useState } from 'react';
 
+import "./App.css"
 
 import GridEditor from "./components/GridEditor/GridEditor";
 import AddDialog from "./components/AddDialog/AddDialog";
 
-import { BAR_CHART, LINE_CHART, MOCK_BAR_GRAPH_REQUEST_RETURN, MOCK_LINE_GRAPH_REQUEST_RETURN, MOCK_PIE_GRAPH_REQUEST_RETURN, MOCK_TITLE, PIE_CHART } from './types/Constants';
+import { BAR_CHART, LINE_CHART, MOCK_BAR_GRAPH_REQUEST_RETURN, MOCK_LINE_GRAPH_REQUEST_RETURN, MOCK_MULTI_BAR_GRAPH_REQUEST_RETURN, MOCK_MULTI_LINE_GRAPH_REQUEST_RETURN, MOCK_PIE_GRAPH_REQUEST_RETURN, MOCK_TABLE_REQUEST_RETURN, MOCK_TITLE, MULTI_BAR_CHART, MULTI_LINE_CHART, PIE_CHART, TABLE_CHART } from './types/Constants';
 import { DataType, Entity, GraphRequest } from './types/BackendInterfaces';
 import { VizDataProps } from './types/DisplayInterfaces';
 
